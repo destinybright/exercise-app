8dd40a28cab4496ddce4e7df12ef63716131d8e3
destinybright <130610038+destinybright@users.noreply.github.com>
2024-09-16 16:57:56 -0700

    Readability changes

--- exercise-mern/exercises-react/src/components/Header.jsx
+++ exercise-mern/exercises-react/src/components/Header.jsx
@@ -4,9 +4,7 @@ function CreateHeader() {
     return (
         <header className="App-header">
             <h1>Exercise Tracker</h1>
-            <p>
-            Full Stack MERN App Demonstration
-            </p>
+            <p>Full Stack MERN App Demonstration</p>
         </header>
     );
 };
